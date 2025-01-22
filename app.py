5b163ef42d89f061b1c8f9be90d78e921cc1e1c0
yanxunlong <167728341+yanxunlong@users.noreply.github.com>
2025-01-22 20:15:23 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+import streamlit as st
+import pandas as pd
 import joblib
 import requests
-import streamlit as st
 
 def download_model(url, output_path):
     response = requests.get(url)
@@ -29,20 +30,38 @@ fuel_consumption = st.number_input("Fuel Consumption (g/km):", min_value=0.0, st
 mileage_in_km = st.number_input("Mileage (in km):", min_value=0.0, step=100.0)
 car_age = st.number_input("Car Age (Years):", min_value=0, step=1)
 
-brands = [col for col in df.columns if col.startswith('brand_')]
+brands = [
+    'brand_alfa-romeo', 'brand_audi', 'brand_bmw', 'brand_cadillac',
+    'brand_chevrolet', 'brand_chrysler', 'brand_citroen', 'brand_dodge',
+    'brand_ferrari', 'brand_fiat', 'brand_ford', 'brand_honda',
+    'brand_hyundai', 'brand_infiniti', 'brand_isuzu', 'brand_jaguar',
+    'brand_jeep', 'brand_kia', 'brand_lada', 'brand_lamborghini',
+    'brand_lancia', 'brand_land-rover', 'brand_maserati', 'brand_mazda'
+]
+colors = [
+    'color_black', 'color_blue', 'color_bronze', 'color_brown',
+    'color_gold', 'color_green', 'color_grey', 'color_orange',
+    'color_red', 'color_silver', 'color_violet', 'color_white',
+    'color_yellow'
+]
+transmissions = [
+    'transmission_type_Manual', 'transmission_type_Automatic',
+    'transmission_type_Semi-automatic', 'transmission_type_Unknown'
+]
+fuel_types = [
+    'fuel_type_Diesel', 'fuel_type_Petrol', 'fuel_type_Hybrid',
+    'fuel_type_Electric', 'fuel_type_Ethanol', 'fuel_type_Hydrogen',
+    'fuel_type_LPG', 'fuel_type_CNG', 'fuel_type_Other'
+]
+
 selected_brand = st.selectbox("Select Brand:", brands)
-brand_data = {col: 1 if col == selected_brand else 0 for col in brands}
-
-colors = [col for col in df.columns if col.startswith('color_')]
 selected_color = st.selectbox("Select Color:", colors)
-color_data = {col: 1 if col == selected_color else 0 for col in colors}
-
-transmissions = [col for col in df.columns if col.startswith('transmission_type_')]
 selected_transmission = st.selectbox("Select Transmission Type:", transmissions)
-transmission_data = {col: 1 if col == selected_transmission else 0 for col in transmissions}
-
-fuel_types = [col for col in df.columns if col.startswith('fuel_type_')]
 selected_fuel_type = st.selectbox("Select Fuel Type:", fuel_types)
+
+brand_data = {col: 1 if col == selected_brand else 0 for col in brands}
+color_data = {col: 1 if col == selected_color else 0 for col in colors}
+transmission_data = {col: 1 if col == selected_transmission else 0 for col in transmissions}
 fuel_type_data = {col: 1 if col == selected_fuel_type else 0 for col in fuel_types}
 
 if st.button("Predict"):
@@ -61,3 +80,4 @@ if st.button("Predict"):
     prediction = model.predict(input_data)[0]
 
     st.success(f"The predicted price is €{prediction:,.2f}")
+
